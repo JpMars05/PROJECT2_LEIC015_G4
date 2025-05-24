b56bfa9bd00f7257b9df94b1834778b82edfac8a
Gonçalo Ferreira <goncalo1133@gmail.com>
2025-05-24 16:59:23 +0100

    fix dynamic programming

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -109,6 +109,7 @@ void dynamicProgrammingKnapsack(const vector<pallet>& pallets, int maxWeight) {
     // Cria uma tabela DP com n+1 linhas e maxWeight+1 colunas, inicializada a zero.
     // dp[i][j] representa o lucro máximo usando as primeiras i paletes com capacidade j.
     vector<vector<int>> dp(n + 1, vector<int>(maxWeight + 1, 0));
+    vector<vector<int>> cnt(n + 1, vector<int>(maxWeight + 1, 0));
 
     // Preenche a tabela DP de forma iterativa
     for (int i = 1; i <= n; ++i) {
@@ -116,33 +117,37 @@ void dynamicProgrammingKnapsack(const vector<pallet>& pallets, int maxWeight) {
         int p = pallets[i - 1].profit; // Lucro da palete atual
 
         for (int j = 0; j <= maxWeight; ++j) {
-            // Se a palete atual couber na capacidade j
-            if (w <= j) {
-                // Escolhe o melhor entre: não usar esta palete (dp[i-1][j])
-                // ou usar esta palete (dp[i-1][j-w] + lucro desta palete)
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + p);
-            } else {
-                // Não dá para usar esta palete, então copia o valor de cima
-                dp[i][j] = dp[i - 1][j];
-            }
+             int bestProfit = dp[i - 1][j];
+             int bestCount  = cnt[i - 1][j];
+
+             if (w <= j) {
+                 int pickProfit = dp[i - 1][j - w] + p;
+                 int pickCount  = cnt[i - 1][j - w] + 1;
+
+                 if (pickProfit > bestProfit ||
+                    (pickProfit == bestProfit && pickCount < bestCount)) {
+                     bestProfit = pickProfit;
+                     bestCount  = pickCount;
+                 }
+             }
+
+             dp[i][j]  = bestProfit;
+             cnt[i][j] = bestCount;
         }
     }
 
-    // Recupera o lucro máximo total a partir da última célula da tabela
-    int res = dp[n][maxWeight];
-    int w = maxWeight; // Capacidade restante
-    vector<int> selected; // IDs das paletes escolhidas
+    int w = maxWeight;
+    vector<int> selected;
 
-    // Percorre a tabela de trás para a frente para descobrir quais paletes foram escolhidas
-    for (int i = n; i > 0 && res > 0; --i) {
-        // Se o valor mudou ao passar de dp[i][w] para dp[i-1][w],
-        // isso significa que esta palete foi incluída na solução ótima
-        if (res != dp[i - 1][w]) {
-            selected.push_back(pallets[i - 1].id);         // Guarda o ID da palete
-            res -= pallets[i - 1].profit;                  // Subtrai o lucro da palete escolhida
-            w -= pallets[i - 1].weight;                    // Subtrai o peso da palete
-        }
+    for (int i = n; i > 0; --i) {
+      if (dp[i][w] == dp[i - 1][w] && cnt[i][w] == cnt[i - 1][w]) {
+
+      } else {
+        selected.push_back(pallets[i - 1].id);
+        w -= pallets[i - 1].weight;
+      }
     }
+
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> duration = end - start;
 
